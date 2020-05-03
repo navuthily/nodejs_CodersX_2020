af6f82591d709cbd173ea0d7fc26dfd48deecac2
Glitch (navuthily-yaya-4) <none>
2020-05-03 22:23:08 +0700

    push all files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const port=3000;
+const port=3000 || process.env.PORT;
 require('dotenv').config();
 const cookieParser = require('cookie-parser')
 var routerCart = require('./routes/cart.route')
